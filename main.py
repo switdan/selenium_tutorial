73a7a90aad8490ef9b1d2ef94feb45c53d874eb6
switdan <switala.daniel@gmail.com>
2026-02-21 15:15:30 +0100

    rozwiniecie kodu, klikanie w przycisk, wpisywanie wartosci

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
 from time import sleep
-
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get('https://www.onet.pl')
-sleep(15)
+driver.get('https://automationpractice.techwithjatin.com/')
+
+driver.find_element(By.PARTIAL_LINK_TEXT, "Sign in")
+signup_button = driver.find_element(By.XPATH , '//a[@title="Log in to your customer account"]')
+signup_button.click()
+
+email_registration_field = driver.find_element(By.XPATH , '//*[@id="email_create"]')
+email_registration_field.send_keys("tester@gmail.com")
+
+register_button = driver.find_element(By.XPATH , '/html/body/div/div[2]/div/div[3]/div/div/div[1]/form/div/div[3]/button/span')
+register_button.click()
+sleep(5)
+
+# shadow_host = driver.find_element(By.ID, "usercentrics-root")
+# shadow_root = shadow_host.shadow_root
+# accept_button = shadow_root.find_element(By.CSS_SELECTOR, 'button[data-testid="uc-accept-all-button"]')
